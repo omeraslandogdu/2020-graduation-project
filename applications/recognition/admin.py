68c2f8f32334e64c6df1f8c8f8201fa660e8067d
Ömer Aslandoğdu <omeraslandogdu@omeraslandogdu.local>
2020-01-28 22:17:37 +0300

    Usermodel changed and admin user model changed

--- applications/recognition/admin.py
+++ applications/recognition/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from applications.core.admin import RelatedObjectLinkMixin
-from applications.recognition.models.user import Client, User, ClientToken, UserType
-
+from applications.recognition.models.user import User, UserToken, UserType
+from django.contrib.auth.admin import UserAdmin
 
 class BaseModelAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
@@ -9,10 +9,10 @@ class BaseModelAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return qs.filter(deleted_at=None)
 
-        return qs.filter(client=request.user.id, deleted_at=None)
+        return qs.filter(user=request.user.id)
 
 
-@admin.register(ClientToken)
+@admin.register(UserToken)
 class TokenAdmin(admin.ModelAdmin):
     list_display = ('key', 'user', 'created')
     fields = ('user', 'key',)
@@ -25,39 +25,39 @@ class TokenAdmin(admin.ModelAdmin):
         return qs.filter(id=request.user.id)
 
 
-@admin.register(Client)
-class ClientAdmin(BaseModelAdmin):
-    list_display = (
-        'id',
-        'app_name',
-        'app_id',
-        'is_superuser',
-        'is_active',
-    )
+@admin.register(User)
+# The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
 
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'admin')
+    list_filter = ('admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+    
     def get_queryset(self, request):
-        qs = super(ClientAdmin, self).get_queryset(request)
+        qs = super(UserAdmin, self).get_queryset(request)
         if request.user.is_superuser:
             return qs
 
         return qs.filter(id=request.user.id)
-
-
-@admin.register(User)
-class UserAdmin(BaseModelAdmin):
-    list_display = (
-        'id',
-        '__str__',
-        'client',
-        'created_at',
-        'updated_at',
-        'status',
-    )
-    search_fields = ['fullname', 'email']
-    list_filter = (
-        'client',
-        'status',
-    )
 
 
 @admin.register(UserType)
@@ -65,8 +65,8 @@ class UserTypeAdmin(RelatedObjectLinkMixin, BaseModelAdmin):
     list_display = (
         'id',
         'title',
+        'user',
         'entity_type_link',
-        'client',
         'created_at',
         'updated_at',
         'status',
