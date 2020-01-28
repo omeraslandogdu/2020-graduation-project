3eef6778213c7651ea498d64fb6effe2e0b47b52
Ömer Aslandoğdu <omeraslandogdu@omeraslandogdu.local>
2020-01-28 23:10:16 +0300

    prediction view added and deleted .gitignore on static file

--- applications/recognition/management/commands/__init__.py
+++ applications/recognition/management/commands/__init__.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
+
 
 
 @login_required
 @csrf_exempt
-def prediction(request):
+def recognition_view(request):
     if request.method == 'POST':
         photo = request.FILES.get('photo')
 
