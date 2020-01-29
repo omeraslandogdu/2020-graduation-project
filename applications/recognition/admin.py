9e6f75103c3af6691b24a443fd1a9324aaf50ae2
Ömer Aslandoğdu <omeraslandogdu@gmail.com>
2020-01-29 17:00:41 +0300

    .gitignore changes

--- applications/recognition/admin.py
+++ applications/recognition/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from applications.core.admin import RelatedObjectLinkMixin
 from applications.recognition.models.user import User, UserToken, UserType
+from applications.recognition.models.entity_type import EntityType
 from django.contrib.auth.admin import UserAdmin
 from .forms import UserAdminChangeForm, UserAdminCreationForm
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -36,8 +37,15 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'admin')
-    list_filter = ('admin',)
+    list_display = \
+        (
+            'id',
+            'email',
+            'admin',
+            'fullname',
+            'entity_type'
+        )
+    list_filter = ('admin', 'active')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ()}),
@@ -70,6 +78,19 @@ class UserTypeAdmin(RelatedObjectLinkMixin, BaseModelAdmin):
         'title',
         'user',
         'entity_type_link',
+        'created_at',
+        'updated_at',
+        'status',
+    )
+    change_links = ('entity_type',)
+
+
+@admin.register(EntityType)
+class EntityTypeAdmin(RelatedObjectLinkMixin, BaseModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'key',
         'created_at',
         'updated_at',
         'status',
--- applications/recognition/models/entity_type.py
+++ applications/recognition/models/entity_type.py
@@ -10,10 +10,8 @@ __all__ = [
 class EntityType(BaseModel):
     title = models.CharField(max_length=255)
     key = models.CharField(max_length=255, db_index=True, )
-    user = models.ForeignKey('User', models.DO_NOTHING)
 
     class Meta:
-        unique_together = ['user', 'key']
         ordering = ['title']
 
     def __str__(self):
