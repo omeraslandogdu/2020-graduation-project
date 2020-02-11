bc23e5d0cc0974399359b2f2be2a13ffbafb3327
Ömer Aslandoğdu <omeraslandogdu@gmail.com>
2020-02-11 15:19:52 +0300

    heroku deployment

--- applications/settings.py
+++ applications/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -165,3 +166,9 @@ MEDIA_URL = 'media/'
 AUTH_USER_MODEL = "recognition.User"
 
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
